51308842f386b2c0163023d0bce7bccbed74b573
SDayie <senamadayie@gmail.com>
2026-01-23 13:36:35 +0100

    Delete target directory

--- target/release/deps/python_cleaner-6277c9cfc1bffc6d.d
+++
@@ -1,5 +0,0 @@
-/Users/enyo/Downloads/Python File Cleaner CLI Tool/target/release/deps/python_cleaner-6277c9cfc1bffc6d.d: src/main.rs
-
-/Users/enyo/Downloads/Python File Cleaner CLI Tool/target/release/deps/python_cleaner-6277c9cfc1bffc6d: src/main.rs
-
-src/main.rs:
--- target/release/python-cleaner.d
+++
@@ -1,1 +0,0 @@
-/Users/enyo/Downloads/Python\ File\ Cleaner\ CLI\ Tool/target/release/python-cleaner: /Users/enyo/Downloads/Python\ File\ Cleaner\ CLI\ Tool/src/main.rs
