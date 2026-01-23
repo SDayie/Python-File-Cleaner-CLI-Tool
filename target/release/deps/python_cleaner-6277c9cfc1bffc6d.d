e9e47d1e2039a134a53b63db4b7f1c6e9979408d
SDayie <senamadayie@gmail.com>
2026-01-23 13:18:41 +0100

    Initial commit: Rust-based Python cleaner CLI

---
+++ target/release/deps/python_cleaner-6277c9cfc1bffc6d.d
@@ -0,0 +1,5 @@
+/Users/enyo/Downloads/Python File Cleaner CLI Tool/target/release/deps/python_cleaner-6277c9cfc1bffc6d.d: src/main.rs
+
+/Users/enyo/Downloads/Python File Cleaner CLI Tool/target/release/deps/python_cleaner-6277c9cfc1bffc6d: src/main.rs
+
+src/main.rs:
---
+++ target/release/python-cleaner.d
@@ -0,0 +1,1 @@
+/Users/enyo/Downloads/Python\ File\ Cleaner\ CLI\ Tool/target/release/python-cleaner: /Users/enyo/Downloads/Python\ File\ Cleaner\ CLI\ Tool/src/main.rs
